ca9df4fd71fe16a93b1b7478d9dd32cef3f5e756
Pedro Souza <pedro.souza4237@gmail.com>
2024-05-24 13:58:05 -0300

    Started a task manager

---
+++ MODULO_1/projeto/gerenciador.py
@@ -0,0 +1,17 @@
+
+
+while True:
+    print("\nMenu gerenciador de lista de tarefas")
+    print("1 - Adicionar tarefa")
+    print("2 - Ver tarefas")
+    print("3 - Atualizar tarefa")
+    print("4 - Completar tarefa")
+    print("5 - Remover tarefas completadas tarefa")
+    print("6 - Sair")
+    print("\n")
+    escolha = int(input("Escolha a sua escolha: "))
+
+    if escolha == 6:
+        break
+
+print("Fim do programa")
---
+++ MODULO_1/while.py
@@ -0,0 +1,8 @@
+# Exemoplo de uso do laço de repetição while
+a = 0
+
+while a <= 10:
+    print(a)
+    a += 1
+
+print('Fim do loop')
